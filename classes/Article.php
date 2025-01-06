281c91ca00cf7c9ec71588af1f11611ca0e70fde
Mohammed reida <reidamohammed3@gmail.com>
2025-01-06 14:20:25 +0100

    add the method get article to show the articles in the table

--- classes/Article.php
+++ classes/Article.php
@@ -1,11 +1,41 @@
 <?php
 namespace App;
 
-require_once '../config/db.php';
+use App\CRUD;
+require_once dirname(__DIR__) . './vendor/autoload.php'; 
+
 class Article{
     
+    private int $id;
+    private string $title;
+    private string $slug;
+    private string $content;
+    private int $category_id;
+    private string $featured_image;
+    private string $status;
+    private string $scheduled_date;
+    private int $author_id;
+    private string $created_at;
+    private string $updated_at;
+    private int $views;
 
+
+
+    public static function getAllArticles(){
+        $articles =CRUD::select('articles join categories on articles.category_id=categories.id join users on articles.author_id=users.id');
+        return $articles;
+    
+    }
+
+    public static function getCountArticle(){
+        $articles =CRUD::select('articles','count(*) as count');
+        
+            echo($articles[0]['count']);
+            
+       
+    }
 
 }
 
 ?>
+
