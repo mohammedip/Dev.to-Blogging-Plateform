89aa7ced4e6b6d6e192e3f94e59749fe83f65c7b
Mohammed reida <reidamohammed3@gmail.com>
2025-01-08 18:00:17 +0100

    add sidebar

--- View/Forms/UserAdd.php
+++ View/Forms/UserAdd.php
@@ -5,15 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users</title>
     <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../pages/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../pages/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="../pages/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 <body class="d-flex align-items-center" style="min-height: 100vh;">
+
+<?php  include '../pages/components/sidebar.php'; ?>
 
 <div class="container" style="width: 50%;">
     <h2 class="text-center mb-4">Formulaire des Utilisateurs</h2>
 
     <!-- Form for creating, updating, and deleting users -->
-    <form action="../../classes/User.php" method="POST">
+    <form action="../../classes/User.php?function=add" method="POST">
         <div class="form-group mb-4">
             <label for="username">Username :</label>
             <input type="text" class="form-control" id="username" name="username" placeholder="Enter username" required>
@@ -34,14 +45,7 @@
             <label for="profile_picture_url">Profile Picture URL :</label>
             <input type="text" class="form-control" id="profile_picture_url" name="profile_picture_url" placeholder="Enter profile picture URL">
         </div>
-        <div class="form-group mb-4">
-            <label for="role">Role :</label>
-            <select class="form-control" id="role" name="role">
-                <option value="user">User</option>
-                <option value="auteur">Auteur</option>
-                <option value="admin">Admin</option>
-            </select>
-        </div>
+        
 
         <!-- Buttons centered -->
         <div class="d-flex justify-content-center">
@@ -54,6 +58,21 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="../pages/vendor/jquery/jquery.min.js"></script>
+    <script src="../pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../pages/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../pages/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../pages/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../pages/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../pages/js/demo/datatables-demo.js"></script>
 
 </body>
 </html>
--- View/Forms/UserUpdate.php
+++ View/Forms/UserUpdate.php
@@ -1,3 +1,11 @@
+<?php
+use App\User;
+
+
+require_once '../../vendor/autoload.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +13,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users</title>
     <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../pages/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../pages/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="../pages/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 <body class="d-flex align-items-center" style="min-height: 100vh;">
+
+<?php  include '../pages/components/sidebar.php'; ?>
 
 <div class="container" style="width: 50%;">
     <h2 class="text-center mb-4">Formulaire des Utilisateurs</h2>
 
     <!-- Form for creating, updating, and deleting users -->
-    <form action="../../classes/User.php" method="POST">
+    <form action="../../classes/User.php?function=update" method="POST">
         <div class="form-group mb-4">
             <label for="username">Username :</label>
             <input type="text" class="form-control" id="username" name="username" placeholder="Enter username" required>
+            <input type="text" class="form-control d-none" id="id" name="id">
         </div>
         <div class="form-group mb-4">
             <label for="email">Email :</label>
@@ -49,11 +69,38 @@
         </div>
     </form>
 </div>
+<?php
+$user=User::getUser($_GET['id']);
+foreach ($user as $user) {
 
+    echo'
+    <script>
+    document.getElementById("username").value = "' . $user['username'] . '";
+        document.getElementById("email").value = "' . $user['email'] . '";
+        document.getElementById("bio").value = "' . $user['bio'] . '";
+        document.getElementById("profile_picture_url").value = "' . $user['profile_picture_url'] . '";
+        document.getElementById("role").value = "' . $user['role'] . '";
+        document.getElementById("id").value = "' . $_GET['id'] . '";
+    </script>
+    ';
+ }
+?>
 <!-- Bootstrap JS and dependencies -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="../pages/vendor/jquery/jquery.min.js"></script>
+    <script src="../pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../pages/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../pages/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../pages/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../pages/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../pages/js/demo/datatables-demo.js"></script>
 
 </body>
 </html>
