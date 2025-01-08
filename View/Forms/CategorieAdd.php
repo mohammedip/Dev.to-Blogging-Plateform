b9b9af47cf90343d8622a301b3e0414ae49fa9e5
Mohammed reida <reidamohammed3@gmail.com>
2025-01-08 17:55:52 +0100

    fix some bugs

--- View/Forms/CategorieAdd.php
+++ View/Forms/CategorieAdd.php
@@ -5,9 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categories</title>
     <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../pages/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../pages/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="../pages/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 <body class="d-flex align-items-center" style="min-height: 100vh;">
+
+<?php  include '../pages/components/sidebar.php'; ?>
 
 <div class="container" style="width: 50%;">
     <h2 class="text-center mb-4">Formulaire des Catégories</h2>
@@ -27,9 +38,21 @@
 </div>
 
 <!-- Bootstrap JS and dependencies -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="../pages/vendor/jquery/jquery.min.js"></script>
+    <script src="../pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../pages/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../pages/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../pages/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../pages/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../pages/js/demo/datatables-demo.js"></script>
 
 </body>
 </html>
